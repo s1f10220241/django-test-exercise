2c753cfae3ac9b1f5dd2efcb02ed1de83d763976
1F10220241 <s1f102202415@iniad.org>
2023-07-12 23:12:01 +0900

    修正１

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
@@ -9,6 +11,7 @@ class Task(models.Model):
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
 
+    
     def is_overdue(self, dt):
         if self.due_at is None:
             return False
